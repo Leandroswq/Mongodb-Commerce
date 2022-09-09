cbae8dbd09b5383fec0fc250c4a5c91ad2e9294d
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-09 12:14:29 -0300

    feat: Desafio 17
    Cria uma nova coleção resumoProdutos para armazenar a quantidade total
    de produtos

---
+++ challenges/desafio17.js
@@ -0,0 +1,9 @@
+db.resumoProdutos.insertOne({
+  franquia: "McDonalds",
+  totalProdutos: db.produtos.countDocuments({}),
+});
+
+db.resumoProdutos.findOne({},
+{ _id: false,
+franquia: true,
+totalProdutos: true });
