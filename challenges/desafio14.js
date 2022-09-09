8579b4b9e221d95f8c7d4d2dcdeb3751c19dc172
Leandroswq <leandrobonfim15@hotmail.com>
2022-09-09 11:26:35 -0300

    feat: Desafio 14
    Retorna os lanches que possuem o ingrediente picles, exibindo apenas o
    nome, ingredientes e os 3 primeiros valores nutricionais

---
+++ challenges/desafio14.js
@@ -0,0 +1,13 @@
+db.produtos.find({
+  ingredientes: {
+    $elemMatch: { $eq: "picles" },
+  },
+},
+{
+  _id: false,
+  nome: true,
+  ingredientes: true,
+  valoresNutricionais: {
+    $slice: 3,
+  },
+});
